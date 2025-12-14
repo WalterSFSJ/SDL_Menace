4c694db26e2b4958cb17cded1e95e1b1e1a50a54
NilBG <nil.barrero@estudiant.enti.cat>
2025-12-14 16:01:34 +0100

    Time Manager

--- source/VerticalMedusa.h
+++ source/VerticalMedusa.h
@@ -1,41 +1,28 @@
 #pragma once
-#include <chrono>
 #include "Enemy.h"
 
-class HorizontalMedusa : public Enemy
+class VerticalMedusa : public Enemy
 {
 public:
 
-	float moveY = 10;
-	const std::chrono::duration<float> moveDuration = std::chrono::seconds(3);
-	std::chrono::steady_clock::time_point time;
+	float moveY = 0.5;
 
-	void RestartMovement() {
-		time = std::chrono::steady_clock::now();
-	}
-
-	HorizontalMedusa()
-		: Enemy(Vector2(RM->WINDOW_WIDTH + 100, (rand() % (RM->WINDOW_HEIGHT - 200)) + 100))
+	VerticalMedusa()
+		: Enemy(Vector2(rand()% 1000 + 100, 100 ))
 	{
 
 		//GetRigidBody()->SetVelocity(	Vector2(		((float)(rand() % 41) + 10), (float)0		)	);
 	}
 
-	void MoveVertical(float deltaTime) {
+	void MoveVertical() {
 
-		std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
-		std::chrono::duration<float> crono = currentTime - time;
-
-		if (crono < moveDuration)
-		{
 			_transform->position.y -= moveY;
-		}
 		
 	}
 
-	void Update(float deltaTime) {
+	void Update() {
 
-		MoveVertical(deltaTime);
+		MoveVertical();
 
 		Enemy::Update();
 
