3186eba81a550e64c87702499ef609f1a58a9590
NilBG <nil.barrero@estudiant.enti.cat>
2026-01-17 20:32:15 +0100

    Change image

--- source/PowerUp.h
+++ source/PowerUp.h
@@ -21,10 +21,20 @@ public:
 		: ImageObject("resources/images/score.png", Vector2(0, 0), Vector2(512.0f, 512.0f))
 	{
 		_transform->position = Vector2(300, 300);
+
+		std::vector<std::string> texturas = {
+		"resources/images/score.png", 
+		"resources/images/laser.png",
+		"resources/images/canon.png",
+		"resources/images/healt.png",
+	    "resources/images/torrets.png",
+		"resources/images/speed.png"
+		};
+
 		for (int i = 0; i < maxLevel; i++)
 		{
 			//WE CONSIDER SPRITES ARE ARRANGED IN A LINE AND ARE 128x64
-			renderers.push_back(new ImageRenderer(this->_transform, "resources/images/score.png", Vector2(0, 0), Vector2(512.0f, 512.0f)));
+			renderers.push_back(new ImageRenderer(this->_transform, texturas[i], Vector2(0, 0), Vector2(512.0f, 512.0f)));
 		}
 		delete _renderer;
 		_renderer = renderers[lvl];
