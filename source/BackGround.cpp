7ecd6f545c1c0f26e8f6316eee7d3a60f3479072
WalterScherk <walter.scherk@enti.cat>
2026-01-27 00:11:01 +0100

    Passable looping

--- source/BackGround.cpp
+++ source/BackGround.cpp
@@ -4,7 +4,10 @@ void BackGround::Update()
 {
 	_transform->position.x -= 1.0f;
 
-
+	if (_transform->position.x < -600.0f)
+	{
+		_transform->position.x = RM->WINDOW_WIDTH / 2 * 3;
+	}
 
 	Object::Update();
 }
