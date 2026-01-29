86cb599f162740d358f9ee9ed6ceea0f3c8e610b
WalterScherk <walter.scherk@enti.cat>
2026-01-29 16:34:30 +0100

    minor chainsaw Changes

--- source/TurboChainsaw.h
+++ source/TurboChainsaw.h
@@ -6,10 +6,10 @@
 class TurboChainsaw : public Enemy {
 public:
     TurboChainsaw(Vector2 spawnPos, float orbitRadius, float orbitSpeed, float orbitDuration)
-        : Enemy(spawnPos, "resources/images/chainsaw.png", Vector2(512.f, 512.f))
+        : Enemy(spawnPos, "resources/images/bubble.png", Vector2(512.f, 512.f))
     {
         score = 300;
-
+        speed = 1.0f;
         Vector2 targetCenter = Vector2(RM->WINDOW_WIDTH / 2.0f, RM->WINDOW_HEIGHT / 2.0f);
 
         esm->AddState(new LinearMovementState(speed, spawnPos, targetCenter, _transform));
